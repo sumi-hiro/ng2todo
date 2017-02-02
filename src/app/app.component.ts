80d4b579a67f6a378db819546292ee7d0d47ab5e
Hiroyuki Suminoue <suminoue.hiro@jp.fujitsu.com>
2017-02-02 22:11:33 +0900

    一旦コミット

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { TodoComponent } from './todo/';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  // templateUrl: 'todo/todo.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hello Angular2 !!';
+  // todos: eny;
+  // constructor() {
+  //   // アプリ内で利用するTodoリストオブジェクト
+  //   this.todos = [];
+  // }
+  // // キーイベントハンドラ
+  // enterTodo(newTodo) {
+  //   var text = newTodo.value.trim();
+  //   if(text) {
+  //     this.addTodo(text);
+  //     newTodo.value = '';
+  //   }
+  // }
+  // // 新しいtodoを追加
+  // addTodo(text) {
+  //   this.todos.push({
+  //     title: text,
+  //     completed: false
+  //   });
+  // }
 }
